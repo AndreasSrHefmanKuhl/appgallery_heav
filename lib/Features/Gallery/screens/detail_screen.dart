12520c83c3ebe8e7c395bc7c0193443ceecc7b29
AndreasKuhl <hefbolzen@gmail.com>
2024-10-09 21:11:45 +0200

    finished detailed screen with data implement, fixed classs gallery Item

--- lib/Features/Gallery/screens/detail_screen.dart
+++ lib/Features/Gallery/screens/detail_screen.dart
@@ -2,17 +2,24 @@ import 'package:app/features/gallery/models/gallery_item.dart';
 import 'package:flutter/material.dart';
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key, required this.item});
   final GalleryItem item;
+  const DetailScreen({super.key, required this.item});
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Image.asset(),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Image.asset(item.imagePath),
+            const SizedBox(height: 8),
+            Text('geschossen am:${item.imageDate}'),
+            const SizedBox(height: 8),
+            Text(item.imageDescription),
+          ],
+        ),
       ),
     );
   }
