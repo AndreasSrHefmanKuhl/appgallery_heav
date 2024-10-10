535e2d7dd0c716112ee86d310b85afd363fe654a
AndreasKuhl <hefbolzen@gmail.com>
2024-10-10 16:59:28 +0200

    made shiny gallery card

--- lib/Features/Gallery/Widgets/gallery_card.dart
+++ lib/Features/Gallery/Widgets/gallery_card.dart
@@ -1,4 +1,6 @@
 import 'package:app/features/gallery/models/gallery_item.dart';
+import 'package:app/features/gallery/screens/detail_screen.dart';
+import 'package:app/features/gallery/widgets/shared/Widget/neumorphic_button.dart';
 import 'package:flutter/material.dart';
 
 class GalleryCard extends StatelessWidget {
@@ -11,22 +13,35 @@ class GalleryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 10),
-            child: SizedBox(
-                width: 150,
-                height: 150,
-                child: Image.asset(
-                  item.imagePath,
-                  fit: BoxFit.cover,
-                  filterQuality: FilterQuality.high,
-                )),
-          ),
-          Text(item.imageTitle)
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: NeumorphicButton(
+        bottomRightShadowSpreadRadius: 2,
+        bottomRightShadowBlurRadius: 2,
+        borderWidth: 5,
+        width: 120,
+        height: 120,
+        backgroundColor: const Color.fromARGB(0, 255, 255, 255),
+        bottomRightShadowColor: const Color.fromARGB(236, 247, 24, 24),
+        topLeftShadowColor: const Color.fromARGB(255, 74, 240, 19),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => DetailScreen(item: item),
+            ),
+          );
+        },
+        child: Column(
+          children: [
+            SizedBox(
+                height: 100,
+                width: 100,
+                child: Image.asset(item.imagePath,
+                    fit: BoxFit.cover, filterQuality: FilterQuality.high)),
+            Text(item.imageTitle),
+          ],
+        ),
       ),
     );
   }
--- lib/Features/Gallery/screens/grid_screen.dart
+++ lib/Features/Gallery/screens/grid_screen.dart
@@ -1,6 +1,6 @@
 import 'package:app/features/gallery/widgets/gallery_card.dart';
 import 'package:app/features/gallery/models/gallery_item.dart';
-import 'package:app/features/gallery/screens/detail_screen.dart';
+
 import 'package:flutter/material.dart';
 
 class GridScreen extends StatelessWidget {
@@ -9,30 +9,17 @@ class GridScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 10),
-      child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 15,
-            mainAxisSpacing: 15,
-          ),
-          itemCount: galleryData.length,
-          itemBuilder: (context, int index) {
-            final item = galleryData[index];
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DetailScreen(item: item),
-                  ),
-                );
-              },
-              child: GalleryCard(
-                item: item,
-              ),
-            );
-          }),
-    );
+        padding: const EdgeInsets.only(top: 10),
+        child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 5,
+              mainAxisSpacing: 5,
+            ),
+            itemCount: galleryData.length,
+            itemBuilder: (context, int index) {
+              final item = galleryData[index];
+              return GalleryCard(item: item);
+            }));
   }
 }
